dca93967e6e83dea5b846289a920a7f35e757cc5
djcavei <martignago2@gmail.com>
2022-10-15 11:54:20 +0200

    initial commit

---
+++ src/main/java/com/musicschool/MusicSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.musicschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musicschool/MusicSchoolApplicationTests.java
@@ -0,0 +1,15 @@
+package com.musicschool;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertEquals(1, 1);
+    }
+
+}
